e1c39e7c53855641997bac8b586e77c71a9b60d0
kukun99 <naka99@gmail.com>
2015-11-16 04:59:40 +0000

    add quantity to line_items

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -24,14 +24,17 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
+    session[:counter] = 0
+
     @cart = current_cart
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product.id)
+    # @line_item = @cart.line_items.build(product: product)
     # @line_item = LineItem.new(line_item_params)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: '商品をカートに入れました。' }
+        format.html { redirect_to @line_item.cart }
         # format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
         format.json { render :show, status: :created, location: @line_item }
       else
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,15 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+
+    if session[:counter].nil?
+      session[:counter] = 1
+    else
+      session[:counter] += 1
+    end
+
+    if session[:counter] >= 5
+      @counter = "<p>あなたはこのページを#{session[:counter]}回訪問しています。</p>"
+    end
   end
 end
